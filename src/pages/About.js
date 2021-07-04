0a332105fdb988a25c1055dc6983a5b75f5f32bb
unknown <pfajemilo@gmail.com>
2021-07-04 12:07:42 -0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section  className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p> loerm , ipsum dolor sit amet consecteturadipsicing eli. Eos doloribus
+        animi pariatur dignissimos nulla lure dolorum et fugit minus hic.
+        Adipisci sint enim, sit qup imperdt cxdtyfu dolmjubuvtcribu accusantium, ipsum esse dvuhkbhiniuffjbntd accusamus ;laboranufvbj 
+       loerm , ipsum dolor sit amet consecteturadipsicing eli. Eos doloribus
+        animi pariatur dignissimos nulla lure dolorum et fugit minus hic.
+        Adipisci sint enim, sit qup imperdt cxdtyfu dolmjubuvtcribu accusantium, ipsum esse dvuhkbhiniuffjbntd accusamus ;laboranufvbj 
+      </p>
+    </section >
+  )
+}
+
+export default About
